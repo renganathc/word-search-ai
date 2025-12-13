430dc1ea3633e88e1c54d348b57b8bad2b53b98c
Renganath Chokkalingam <rengnath1234@gmail.com>
2025-12-13 08:58:13 +0530

    update CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],   # im allowin all origins for now
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["Total-Contours-Before-Denoising", "Total-Letters"] #log purposes
